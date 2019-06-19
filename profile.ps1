a78a8793e9cae491860571a4a2961ffb7ab8b5f8
Corey W. Colvin <ccolvin@esu2.org>
2019-06-19 10:49:12 -0500

    added pending reboot function

--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,7 @@
 #
 #
 #    Changelog:
+#         06/19/19 - Added Function Test-PendingReboot
 #         04/16/10 - Got rid of annoying EXO connecting at startup
 #         04/10/19 - Forked from Codys profile (props to Cody)
 #         11/04/18 - Added alias for Get-Command as gcmd
@@ -392,6 +393,22 @@ function Connect-SecurityAndComplianceCenter {
 #                                             Resources
 #
 #############################################################################################################
+
+function Test-PendingReboot
+{
+ if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
+ if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
+ if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+ try { 
+   $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+   $status = $util.DetermineIfRebootPending()
+   if(($status -ne $null) -and $status.RebootPending){
+     return $true
+   }
+ }catch{}
+ 
+ return $false
+}
 
 function Get-ComputerUptime {
     Param(
